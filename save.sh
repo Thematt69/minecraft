dfca5e132790ad6a80a71f0c7547631dcee1b7cc
Matthieu Devilliers <devilliers.matthieu@gmail.com>
2021-02-22 23:36:20 +0100

    Save 22/02/2021-23:36

--- save.sh
+++ save.sh
@@ -8,10 +8,21 @@ YELLOW="\033[1;93m"
 RED="\033[1;31m"
 GREEN="\033[1;32m"
 
-echo -e "[$YELLOW "Git^^" "\e[0m"] : "" "Git pull --autostash" "\e[0m""
+printer(){
+	local display_name=$1
+	local msg=$2
+	local color_display_name=$3
+	local color_msg=$4
+	local end_color="\e[0m"
+
+	echo -e "[$color_display_name ${display_name^^} $end_color] : $color_msg $msg $end_color"
+}
 
 # Récupération du GitHub
+printer "Git" "Git pull --autostash" "$YELLOW" ""
 git pull --autostash
+
+sleep 1
 
 # Ajout de tous les nouveaux fichiers sur le git
 git add .
