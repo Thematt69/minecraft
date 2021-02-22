4029eeeaa3d7766e71dc02cd82c8c587bfe712da
Matthieu Devilliers <devilliers.matthieu@gmail.com>
2021-02-23 00:48:50 +0100

    Finish script bash

--- save.sh
+++ save.sh
@@ -3,7 +3,9 @@
 VERSION=1.0
 AUTHOR="Matthieu DEVILLIERS <matthieu@devilliers.fr>"
 LOGDIR=logs
-DATE=$(date +%d-%m-%Y_%H:%M)
+DATE=$(date +%d-%m-%Y)
+TIME=$(date +%H:%M)
+DATETIME="$DATE - $TIME"
 
 #colors
 YELLOW="\033[1;93m"
@@ -18,15 +20,17 @@ printer(){
 
 	echo -e "[$color_display_name ${display_name^^} $end_color] : $color_display_name $msg $end_color"
 }
+echo $DATETIME
 
 # Récupération du GitHub
 printer "Git" "Récupération des dernières modifications..." "$YELLOW"
-git pull --autostash >> "$LOGDIR"/git/"$DATE".log 2>&1
+git pull --autostash >> "$LOGDIR"/git/"$DATETIME".log 2>&1
 
 # Ajout de tous les nouveaux fichiers sur le git
 printer "Git" "Ajout des fichiers non répertorié..." "$YELLOW"
-git add . >> "$LOGDIR"/git/"$DATE".log 2>&1
+git add . >> "$LOGDIR"/git/"$DATETIME".log 2>&1
 
+# Regarde si il y a des changements avec le dernier commit
 if [ -n "$(git status --porcelain)" ]; then
     printer "Git" "Changements trouvées !" "$RED"
     read -p 'Voulez-vous continuer ? (y/n)' response
@@ -39,11 +43,11 @@ else
 fi
 
 # Commit sur GitHub
-printer "Git" "Préparation du commit" "$YELLOW"
-git commit -m "Save $DATE" >> "$LOGDIR"/git/"$DATE".log 2>&1
+printer "Git" "Préparation du commit..." "$YELLOW"
+git commit -m "Save $DATETIME" >> "$LOGDIR"/git/"$DATETIME".log 2>&1
 
 # Push sur GitHub
 printer "Git" "Envoi vers GitHub..." "$RED"
-git push >> "$LOGDIR"/git/"$DATE".log 2>&1
+git push >> "$LOGDIR"/git/"$DATETIME".log 2>&1
 
 printer "Git" "Sauvegarde terminé !" "$GREEN"
